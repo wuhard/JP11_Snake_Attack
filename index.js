39e34594bb53a589705fb73ea7b71bc05404eaec
wuhard <7533935+wuhard19900104@user.noreply.gitee.com>
2024-10-08 11:21:11 +0800

    new codes

---
+++ index.js
@@ -0,0 +1,1 @@
+window.screenOrientation="sensor_landscape",loadLib("libs/min/laya.core.min.js"),loadLib("libs/min/laya.ui.min.js"),loadLib("libs/min/laya.d3.min.js"),loadLib("libs/min/W.min.js"),loadLib("libs/min/ad.min.js"),loadLib("libs/min/fairygui.min.js"),loadLib("libs/min/laya.physics3D.min.js"),loadLib("libs/min/utils.min.js"),loadLib("js/bundle.js");
